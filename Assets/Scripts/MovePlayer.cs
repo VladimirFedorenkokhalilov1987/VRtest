e23bbf6a5d97163f9aeef0e87a9cf21428fb73b6
Владимир Федоренко-Халилов <fedorenkokhalilov@gmail.com>
2019-07-30 13:05:06 +0300

    Init commit

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public int _playerSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+            transform.position= transform.position+ Camera.main.transform.forward*_playerSpeed*Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+            Application.Quit(); 
+    }
+}
---
+++ Assets/Scripts/SomeItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeItem : MonoBehaviour
+{
+
+    public float spinValue =90;
+    public Material _thisMaterial;
+    public AudioSource _thisAudio;
+    private Renderer _renderer;
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up*spinValue*Time.deltaTime);
+    }
+
+    public void OnPointerIn()
+    {
+        _renderer.material.SetColor("_Color", Color.blue);
+    }
+
+    public void OnPointerOut()
+    {
+        _renderer.material.SetColor("_Color", _thisMaterial.color);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            _thisAudio.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _thisAudio.Stop();
+        }
+    }
+}
